1d1bcba545d6e154cdf6a9dcb09612f99ea5482e
Jenny <156132099+Jennyznz@users.noreply.github.com>
2025-11-04 12:37:00 -0500

    Add navigation buttons to slideshow

--- main.js
+++ main.js
@@ -17,10 +17,34 @@ function displaySlides() {
     const carousel = document.getElementById('img-carousel');
     const slides = document.querySelectorAll('.slide');
 
-    // There are less slides than gallery items
-    slides.forEach((slide, index )=> {
-        const img = document.createElement('img');
+    slides.forEach((slide, index )=> {  // There are less slides than gallery items
+        const img = slide.querySelector('img');
         img.src = gallery[index];
-        slide.append(img)
     });
+}
+
+const prevBtn = document.getElementById('left');
+prevBtn.addEventListener('click', () => {
+    prevSlide();
+});
+
+function prevSlide() {
+    // Move last image in gallery to the front
+    const last = gallery.pop();
+    gallery.unshift(last);
+
+    displaySlides();
+}
+
+const nextBtn = document.getElementById('right');
+nextBtn.addEventListener('click', () => {
+    nextSlide();
+})
+
+function nextSlide() {
+    // Move first image in gallery to the end
+    const first = gallery.shift();
+    gallery.push(first);
+
+    displaySlides();
 }
